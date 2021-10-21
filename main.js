6e5fca50b7e43a37673cea0335f7186a3638a9d2
鈴木　大輝 <shiningbless@icloud.com>
2021-10-21 12:11:46 +0900

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD9FmWd3qn9dAEtqIXstAAOox4YFtIRLm8',
+  authDomain: 'doyouhave-muscle.firebaseapp.com',
+  projectId: 'doyouhave-muscle',
+  storageBucket: 'doyouhave-muscle.appspot.com',
+  messagingSenderId: '444191949429',
+  appId: '1:444191949429:web:0f2cb5c0880aff47edae3c'
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
